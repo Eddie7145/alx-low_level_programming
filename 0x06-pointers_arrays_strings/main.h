c75f35cd69be452692f0bbc9facbd71e867df85c
Eddie7145 <amaachukwuchizzy@gmail.com>
2022-09-21 18:07:29 -0700

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -9,3 +14,5 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
+
+#endif /* MAIN_H */
